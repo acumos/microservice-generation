39deb7a089635de4952834047b801e2e8b869bd9
Jabeen Shaikh <js00353493@techmahindra.com>
2018-07-03 15:27:41 +0530

    Initial commit
    Change-Id: I4a30218cdfe0dc33651fda875ee52ad12c11c9f4
    Issue-ID: ACUMOS-1252
    Signed-off-by: Jabeen Shaikh <js00353493@techmahindra.com>

---
+++ src/main/java/org/acumos/microservice/MicroserviceApplication.java
@@ -0,0 +1,44 @@
+package org.acumos.microservice;
+
+import org.acumos.onboarding.common.utils.EELFLoggerDelegate;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootApplication
+@ComponentScan("org.acumos.microservice")
+public class MicroserviceApplication implements ApplicationContextAware {
+	private static final EELFLoggerDelegate logger = EELFLoggerDelegate.getLogger(MicroserviceApplication.class);
+
+	public static final String CONFIG_ENV_VAR_NAME = "SPRING_APPLICATION_JSON";
+
+	
+	
+	public static void main(String[] args) throws Exception {
+		final String springApplicationJson = System.getenv(CONFIG_ENV_VAR_NAME);
+
+		if (springApplicationJson != null && springApplicationJson.contains("{")) {
+			final ObjectMapper mapper = new ObjectMapper();
+			// ensure it's valid
+			mapper.readTree(springApplicationJson);
+			logger.info("Successfully parsed configuration from environment {" + CONFIG_ENV_VAR_NAME + "}");
+		} else {
+
+			logger.warn("No configuration found in environment {" + CONFIG_ENV_VAR_NAME + "}");
+		}
+		
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		((ConfigurableEnvironment) context.getEnvironment()).setActiveProfiles("src");
+	}
+
+}
