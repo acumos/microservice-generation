4623e3cff7220293b2f35920a29f636d5af389ba
Jabeen Shaikh <js00353493@techmahindra.com>
2018-10-04 15:57:07 +0530

    Onap artifact changes
    Change-Id: I2c654005ce617a15ec7028c2bb93f04a6e8e80ea
    Issue-ID: ACUMOS-1639
    Signed-off-by: Jabeen Shaikh <js00353493@techmahindra.com>

--- src/main/java/org/acumos/microservice/services/impl/GenerateMicroserviceController.java
+++ src/main/java/org/acumos/microservice/services/impl/GenerateMicroserviceController.java
@@ -118,7 +118,6 @@ public class GenerateMicroserviceController extends DockerizeModel implements Do
 
 		String artifactName = null;
 		List<String> artifactNameList = new ArrayList<String>();
-		File files = null;
 		Metadata mData = null;
 		OnboardingNotification onboardingStatus = null;
 		MLPSolution mlpSolution = new MLPSolution();
@@ -189,7 +188,9 @@ public class GenerateMicroserviceController extends DockerizeModel implements Do
 			
 			logger.debug(EELFLoggerDelegate.debugLogger,"Starting Microservice Generation");
 
-			files = new File("model");
+			String modelId = UtilityFunction.getGUID();
+			File outputFolder = new File("tmp", modelId);
+			outputFolder.mkdirs();
 
 			MultipartFile model = null, meta = null, proto = null;
 			
@@ -198,13 +199,13 @@ public class GenerateMicroserviceController extends DockerizeModel implements Do
 			for (String name : artifactNameList) {
 				if (name.contains(".json")) {
 					logger.debug(EELFLoggerDelegate.debugLogger, "MetaFile: {}", name);
-					MetaFile = new File(files, name);
+					MetaFile = new File(outputFolder, name);
 				} else if (name.contains(".proto")) {
 					logger.debug(EELFLoggerDelegate.debugLogger, "ProtoFile: {}", name);
-					protoFile = new File(files, name);
+					protoFile = new File(outputFolder, name);
 				} else {
 					logger.debug(EELFLoggerDelegate.debugLogger, "ModelFile: {}", name);
-					modelFile = new File(files, name);
+					modelFile = new File(outputFolder, name);
 				}
 			}
 
@@ -286,9 +287,6 @@ public class GenerateMicroserviceController extends DockerizeModel implements Do
 							"Dockerization request recieved with " + model.getOriginalFilename());
 
 					modelOriginalName = model.getOriginalFilename();
-					String modelId = UtilityFunction.getGUID();
-					File outputFolder = new File("tmp", modelId);
-					outputFolder.mkdirs();
 					boolean isSuccess = false;
 					
 					try {
@@ -338,6 +336,8 @@ public class GenerateMicroserviceController extends DockerizeModel implements Do
 						commonOnboarding.addArtifact(mData, imageUri, getArtifactTypeCode("Docker Image"), onboardingStatus);
 						
 						if (deployment_env == 2) {
+                                                        logger.debug(EELFLoggerDelegate.debugLogger, "OutputFolderPath: " + outputFolder);
+                                                        logger.debug(EELFLoggerDelegate.debugLogger, "AbsolutePath OutputFolderPath: " + outputFolder.getAbsolutePath());
 							addDCAEArrtifacts(mData, outputFolder, mlpSolution.getSolutionId(), onboardingStatus);
 						}
 
