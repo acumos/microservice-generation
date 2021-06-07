587fe30c60952e3298d1ff2eb50ecc3d937265d6
Alassane Samba <alassane.samba@orange.com>
2021-06-07 19:35:19 +0200

    consider a .zip file replacing the .bin file
    ISSUE-ID: ACUMOS-4326
    Signed-off-by: Alassane Samba <alassane.samba@orange.com>
    Change-Id: Ib954254102eec2acf812579f9aa998fe71fbe27b

--- src/main/resources/templates/r/run.R
+++ src/main/resources/templates/r/run.R
@@ -1,36 +1,44 @@
 ready_path<-function(path){
   if (dir.exists(path)) {
     given_dir <- path
-    if(!(file.exists(file.path(given_dir, "component.bin"))
-    &&file.exists(file.path(given_dir, "component.proto"))
-	&&file.exists(file.path(given_dir, "meta.json"))
-    &&file.exists(file.path(given_dir, "component.swagger.yaml")))){
+    if(!(
+      (file.exists(file.path(given_dir, "component.bin"))
+       &&file.exists(file.path(given_dir, "component.proto"))
+       &&file.exists(file.path(given_dir, "meta.json"))
+       &&file.exists(file.path(given_dir, "component.swagger.yaml"))) 
+      | 
+      (file.exists(file.path(given_dir, "component.zip"))
+       &&file.exists(file.path(given_dir, "component.proto"))
+       &&file.exists(file.path(given_dir, "meta.json")))
+    )){
       ready_dir <- tempfile("acumos-runtime")
       if (!all(dir.create(ready_dir))) stop("unable to create temporary directory in `",ready_dir,"' to copy and rename the components")
       binFile<-tools::list_files_with_exts(given_dir, exts = 'bin', all.files = FALSE, full.names = TRUE)
+      zipFile<-tools::list_files_with_exts(given_dir, exts = 'zip', all.files = FALSE, full.names = TRUE)
       if(length(binFile)>0){
         fileName<-tools::file_path_sans_ext(binFile)
-        file.link(binFile, file.path(ready_dir, "component.bin"))
+        file.copy(binFile, file.path(ready_dir, "component.bin"))
+      }else if(length(zipFile)>0){
+        fileName<-tools::file_path_sans_ext(zipFile)
+        file.copy(zipFile, file.path(ready_dir, "component.zip"))
       }else{
-        stop('no bin file')
+        stop('no bin or zip file')
       }
       if(file.exists(paste0(fileName,'.proto'))){
         protoFile<-paste0(fileName,'.proto')
-        file.link(protoFile, file.path(ready_dir, "component.proto"))
+        file.copy(protoFile, file.path(ready_dir, "component.proto"))
       }else{
-        stop('no proto file sharing the bin file name')
+        stop('no proto file sharing the bin/zip file name')
       }
       if(file.exists(paste0(fileName,'.json'))){
         jsonFile<-paste0(fileName,'.json')
-        file.link(jsonFile, file.path(ready_dir, "meta.json"))
+        file.copy(jsonFile, file.path(ready_dir, "meta.json"))
       }else{
-        stop('no json file sharing the bin and proto files name')
+        stop('no json file sharing the bin/zip and proto files name')
       }
-	  if(file.exists(paste0(fileName,'.swagger.yaml'))){
-        jsonFile<-paste0(fileName,'.json')
-        file.link(jsonFile, file.path(ready_dir, "component.swagger.yaml"))
-      }else{
-        stop('no swagger.yaml file sharing the json, bin and proto files name')
+      if(file.exists(paste0(fileName,'.swagger.yaml'))){
+        swaggerfile<-paste0(fileName,'.swagger.yaml')
+        file.copy(swaggerfile, file.path(ready_dir, "component.swagger.yaml"))
       }
       target<-ready_dir
     }else{
@@ -39,6 +47,6 @@ ready_path<-function(path){
   }else{
     target<-path
   }
-return(target)
+  return(target)
 }
 acumos:::run(file=ready_path("."), runtime=file.path(".","runtime.json"))
