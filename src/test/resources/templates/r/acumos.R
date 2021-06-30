2fa6ce678b490eab9a62f9d419c372e3b751ffe1
Alassane Samba <alassane.samba@orange.com>
2021-06-30 14:59:28 +0200

    install the acumos-r-client from github
    ISSUE-ID: ACUMOS-4340
    Signed-off-by: Alassane Samba <alassane.samba@orange.com>
    Change-Id: I93f7f20cc851347433746105b53d118811680956

--- src/test/resources/templates/r/acumos.R
+++ src/test/resources/templates/r/acumos.R
@@ -1,1 +1,3 @@
-install.packages("acumos", repos=c("http://cloud.r-project.org","http://rforge.net"), dep=T)
+remotes::install_github("acumos/acumos-r-client", subdir="acumos-package")
+
+#install.packages("acumos", repos=c("http://cloud.r-project.org","http://rforge.net"), dep=T)
