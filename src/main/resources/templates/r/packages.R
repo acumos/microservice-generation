ba1ac9f523f7648b20383302314031543c68ec99
PhilippDo <philippe.dooze@orange.com>
2021-07-02 16:53:01 +0200

    Restore packages.R file
    Issue-Id: ACUMOS-4340
    Change-Id: Ia090337296420864720cad0c2d6ee589fb3b8d8e
    Signed-off-by: PhilippDo <philippe.dooze@orange.com>

--- src/main/resources/templates/r/packages.R
+++ src/main/resources/templates/r/packages.R
@@ -1,7 +1,8 @@
 
-install.packages("bitops",repos="http://cloud.r-project.org",dependencies=T)
-install.packages("R6",repos="http://cloud.r-project.org",dependencies=T)
-install.packages("forecast",repos="http://cloud.r-project.org",dependencies=T)
-install.packages("dplyr",repos="http://cloud.r-project.org",dependencies=T)
-install.packages("lubridate",repos="http://cloud.r-project.org",dependencies=T)
+#install.packages("bitops",repos="http://cloud.r-project.org",dependencies=T)
+#install.packages("R6",repos="http://cloud.r-project.org",dependencies=T)
+#install.packages("forecast",repos="http://cloud.r-project.org",dependencies=T)
+#install.packages("dplyr",repos="http://cloud.r-project.org",dependencies=T)
+#install.packages("lubridate",repos="http://cloud.r-project.org",dependencies=T)
+install.packages("randomForest",repos="http://cloud.r-project.org",dependencies=T)
 
