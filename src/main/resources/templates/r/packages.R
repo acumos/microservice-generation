7894fd99f3d718e53b70ea53f1b20066d6de5e20
PhilippDo <philippe.dooze@orange.com>
2021-07-05 15:53:30 +0200

    update packages.R file
    Issue-Id: ACUMOS-4340
    Change-Id: I862363118dc3d3851ff6d487e9995d4203348b10
    Signed-off-by: PhilippDo <philippe.dooze@orange.com>

--- src/main/resources/templates/r/packages.R
+++ src/main/resources/templates/r/packages.R
@@ -1,8 +1,2 @@
-
-#install.packages("bitops",repos="http://cloud.r-project.org",dependencies=T)
-#install.packages("R6",repos="http://cloud.r-project.org",dependencies=T)
-#install.packages("forecast",repos="http://cloud.r-project.org",dependencies=T)
-#install.packages("dplyr",repos="http://cloud.r-project.org",dependencies=T)
-#install.packages("lubridate",repos="http://cloud.r-project.org",dependencies=T)
-install.packages("randomForest",repos="http://cloud.r-project.org",dependencies=T)
+## This file is populated during the micro-service generation process by all the dependancies listed in meta.json file belonging the model bundle ##
 
